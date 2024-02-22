9f059ae49a63f9728d01de181f74b36e8c591cee
Mateus Bessa <mateusbessacontato@gmail.com>
2024-02-22 14:15:10 -0300

    sending a simple response

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const http = require('http')
+const http = require('http');
+const products = require('./data/products')
 
-const server = http.createServer((req, res) => {})
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(products))
+})
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server runing on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server runing on port ${PORT}`));
