141360a1d050f5b2e8b08bfabfbeb4f73eb2447f
Mateus Bessa <mateusbessacontato@gmail.com>
2024-03-22 15:31:38 -0300

    Creating and configuring the routes

--- N-Layers/src/handler.js
+++ N-Layers/src/handler.js
@@ -1,10 +1,25 @@
 import { parse } from 'node:url'
+import { DEFAULT_HEADER } from './util/util'
+
+const allRoutes = {
+    '/heroes:get': (request, response) => {
+        response.write('GET')
+        response.end()
+    },
+
+    default: (request, response) => {
+        response.writeHead(404, DEFAULT_HEADER)
+        response.write('uuuups, not found!')
+        response.end()
+    }
+}
 
 function handler(request, response) {
     const { url, method } = request
     const { pathname } = parse(url, true)
     const key = `${pathname}:${method.toLowerCase()}`
-    response.end('Hello World!')
+    const chosen = allRoutes[key] || allRoutes.default
+    return chosen(request, response)
 }
 
 export default handler
