00c2be0ed65ea69a0fa1fdd36a267f79dcc83126
Mateus Bessa <mateusbessacontato@gmail.com>
2024-03-22 15:13:52 -0300

    Creating the handler

--- N-Layers/src/index.js
+++ N-Layers/src/index.js
@@ -1,10 +1,10 @@
 import http from 'node:http'
+import handler from './handler.js'
 
 const PORT = process.env.PORT || 3000
 
-const server = http.createServer((request, response) => {
-    response.end('Hello World!')
-}).listen(PORT, () => console.log(`Server is running at ${PORT}`))
+const server = http.createServer(handler)
+    .listen(PORT, () => console.log(`Server is running at ${PORT}`))
 
 export {
     server
