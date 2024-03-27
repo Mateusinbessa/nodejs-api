8120af080912ac5a02d7c5a47dadb79fe427d3cc
Mateus Bessa <mateusbessacontato@gmail.com>
2024-03-27 16:49:30 -0300

    Creating the entity layer

--- N-Layers/src/routes/heroRoute.js
+++ N-Layers/src/routes/heroRoute.js
@@ -1,11 +1,25 @@
+import { once } from 'node:events'
+import { DEFAULT_HEADER } from '../util/util.js'
+import Hero from '../entities/hero.js'
+
 const routes = ({ heroService }) => ({
     '/heroes:get': async (request, response) => {
         response.write('GET')
         response.end()
     },
     '/heroes:post': async (request, response) => {
-        response.write('POST')
-        response.end()
+        //we're gonna get item, once request send some data.
+        const data = await once(request, 'data')
+        const item = JSON.parse(data)
+        const hero = new Hero(item)
+
+        const id = hero.id
+        response.writeHead(201, DEFAULT_HEADER)
+        response.write(JSON.stringify({
+            id: id,
+            success: 'User created with success!!',
+        }))
+        return response.end()
     },
 })
 
